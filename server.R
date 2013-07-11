5441bbe2f59143fd49844f77c058e957ae50518f
kupotti <kupotti@gmail.com>
2013-07-11 13:03:42 +0900

    commit at 201301

---
+++ server.R
@@ -0,0 +1,41 @@
+library(shiny)
+shinyServer(function(input, output) {
+  fig1 <- read.table("table1.txt", head = F, sep = " ")
+  idy <- 1
+  colnames(fig1) <- c("year", "numt", "numm", "numf", "ratet", "ratem", "ratef", "aadrm", "aadrf") 
+  selectedyear <- reactive(function(){
+    idt <- ifelse(input$type=="number", 2, 5)
+    idm <- ifelse(input$type=="number", 3, 6)
+    idf <- ifelse(input$type=="number", 4, 7)
+    ids <- which(fig1$year==input$year)
+    dataset <- data.frame(合計 = fig1[ids,idt], 男性 = fig1[ids,idm], 女性 = fig1[ids,idf])
+    rownames(dataset) <- fig1[ids, 1]
+    dataset
+  })
+  output$distPlot <- reactivePlot(function() {
+    idt <- ifelse(input$type=="number", 2, 5)
+    idm <- ifelse(input$type=="number", 3, 6)
+    idf <- ifelse(input$type=="number", 4, 7)
+    ymax <- ifelse(input$type=="number", 35000, 45)
+    if(input$type=="number"){
+      auxline <- seq(5000, 35000, 5000)
+      ylab <- "自殺死亡数（人）"
+      main <- "第1-a図　自殺死亡の年次推移：自殺死亡数"
+    } else{
+      auxline <- seq(5, 45, 5)
+      ylab <- "自殺死亡率（対10万）"
+      main <- "第1-b図　自殺死亡の年次推移：自殺死亡率"
+    }
+    plot(fig1[, c(idy, idt)], type = "l", main = main, ylim = c(0, ymax), ylab = ylab, xlab = "Year", family="IPAGothic")
+    par(new = T)
+    plot(fig1[, c(idy, idm)], type = "l", col = "blue", ylim = c(0, ymax), ylab = ylab, xlab = "Year", family="IPAGothic")
+    par(new = T)
+    plot(fig1[, c(idy, idf)], type = "l", col = "red", ylim = c(0, ymax), ylab = ylab, xlab = "Year", family="IPAGothic")
+    abline(h = auxline, lty = 2, col = "gray")
+    abline(v = input$year)
+    legend(1950, auxline[length(auxline)], c("総数", "男性", "女性"), lty = 1, col = c("black","blue","red"))
+  })
+  output$data <- reactiveTable(function(){
+    selectedyear()
+  })
+})
