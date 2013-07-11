c513ea38486847bae0fe759ddcbc24a8426d9f66
kupotti <kupotti@gmail.com>
2013-07-11 14:51:29 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,8 +2,8 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel(""),
   sidebarPanel(
-    selectInput("type", "自殺死亡数(number) / 自殺死亡率(rate)", choices = c("number", "rate")),
-    sliderInput("year", "スライダーを移動させて年を変更: ", min = 1950, max = 2010, value = 1998)
+    selectInput("type", "number of suicide/ suicide rate", choices = c("number", "rate")),
+    sliderInput("year", "change year to move slider: ", min = 1950, max = 2010, value = 1998)
   ),
   mainPanel(
     tableOutput("data"), 
